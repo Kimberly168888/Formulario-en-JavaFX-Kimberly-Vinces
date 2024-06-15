d92b5eb27174a483caf571dd63d0225a538bcd8a
Kimberly168888 <Kimy_xvm@hotmail.com>
2024-06-15 00:08:55 -0500

    Create AllControlsController.java

---
+++ AllControlsController.java
@@ -0,0 +1,59 @@
+package javafxcontrols;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
+public class AllControlsController {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private CheckBox checkBox;
+
+    @FXML
+    private Hyperlink hyperlink;
+
+    @FXML
+    private ToggleButton toggleButton;
+
+    @FXML
+    private RadioButton radioButton;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private ProgressIndicator progressIndicator;
+
+    @FXML
+    private Label progressLabel;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Slider slider;
+
+    // Agrega aqui cualquier metodo que necesites para manejar eventos o logica.
+}
